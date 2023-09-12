52631c8eff655914376eae3cc2c9b59c3585fda6
Natalia Biscaro <biscaro321@gmail.com>
2023-09-12 19:20:18 -0300

    Go to edit page

--- cypress-project/cypress/e2e/2-salesforce/simple-login.cy.js
+++ cypress-project/cypress/e2e/2-salesforce/simple-login.cy.js
@@ -5,6 +5,18 @@ describe("Salesforce Simple Login", () => {
   });
 
   it("Just login into Salesforce and check if we load any page.", () => {
-    cy.get(".title.slds-truncate").first().should('have.text', 'Home');
+    cy.get(".title.slds-truncate").first().should("have.text", "Home");
+  });
+
+  it("Go to Edit Page", () => {
+    cy.visit("/lightning/r/0013t00002WCFleAAH/view")
+      .get(".uiMenu")
+      .click()
+      .wait(5000)
+      .get('[role="menu"]')
+      .wait(5000)
+      .get("#edit-page")
+      .click()
+      .wait(5000);
   });
 });
