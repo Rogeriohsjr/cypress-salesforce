48689f7065bdff1c3c599c4c28217c3faf644862
Natalia Biscaro <biscaro321@gmail.com>
2023-09-28 19:29:43 -0300

    Attempt to drag and drop the message.

--- cypress-project/cypress/e2e/2-salesforce/simple-login.cy.js
+++ cypress-project/cypress/e2e/2-salesforce/simple-login.cy.js
@@ -4,11 +4,12 @@ describe("Salesforce Simple Login", () => {
     cy.loginIntoSalesforce();
   });
 
-  it("Just login into Salesforce and check if we load any page.", () => {
+  /*it("Just login into Salesforce and check if we load any page.", () => {
     cy.get(".title.slds-truncate").first().should("have.text", "Home");
-  });
+  });*/
 
-  it("Go to Edit Page", () => {
+  it("Go to Edit Page and put the message on the page", () => {
+    const dataTransfer = new DataTransfer();
     cy.visit("/lightning/r/0013t00002WCFleAAH/view")
       .get(".uiMenu")
       .click()
@@ -17,6 +18,20 @@ describe("Salesforce Simple Login", () => {
       .wait(5000)
       .get("#edit-page")
       .click()
-      .wait(5000);
+      .wait(5000)
+      .get(".sectionBody")
+      .contains("Evm Advanced Message")
+      .as("Message");
+
+    cy.getIframe('[title="Surface"]')
+      .wait(5000)
+      .find('[title="Activity"]')
+      .as("Here")
+      .get("@Message")
+      .drag("@Here"); // I'm using just the "drag" because I'm' using the "cypress-drag-drop" plugin so, the .drag() command will perform both the drag and drop actions so I just need to specify the destination
+
+    //.get("button[type='button']")
+    //.contains("Save")
+    //.click()
   });
 });
